48d230f50222fae03bf2cfbb0cef80522f4a5b3d
Anil Varghese <anil.varghese@care.com>
2015-09-10 09:32:02 +0530

    Using correct provision profile

---
+++ Dictate/Manager/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  Dictate
+//
+//  Created by Anil Varghese on 10/09/15.
+//  Copyright (c) 2015 ThatSoft.com. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+   
+}
